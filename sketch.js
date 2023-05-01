a33a481a1f8ccf8e7c4c78ea5aed386e7d535d0c
m1kayl4 <131909369+m1kayl4@users.noreply.github.com>
2023-05-01 08:28:02 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(playOscillator);
   osc = new p5.Oscillator('sine');
+  textAlign(CENTER);
+  textSize(16);
+  textStyle(BOLD);
 }
 
 function draw() {
@@ -12,9 +15,7 @@ function draw() {
   freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
   amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
 
-  textAlign(CENTER);
-  textSize(16);
-  textStyle(BOLD);
+
   text('tap to play', width/2, height/2.25);
   
   if (playing) {
@@ -22,10 +23,6 @@ function draw() {
     osc.freq(freq, 0.1);
     osc.amp(amp, 0.1);
     background(mouseX/4,mouseY/2,240)
-  
-  textAlign(CENTER);
-  textSize(16);
-  textStyle(BOLD);
   
   //i took this text from the p5.Oscillator reference page but I changed its placement within the canvas/canvas size
  text('tap to play', width/2, height/2.25);
@@ -40,12 +37,6 @@ function playOscillator() {
   playing = true;
 }
 
-if (playing) {
-    // smooth the transitions by 0.1 seconds
-    osc.freq(freq, 0.1);
-    osc.amp(amp, 0.1);
-    background(mouseX/4,mouseY/2,240)
-  }
 
 function mouseReleased() {
   // ramp amplitude to 0 over 0.5 seconds
